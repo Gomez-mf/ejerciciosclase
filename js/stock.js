a33ced0de2ee0b6e32dc07980a60f7aeccaf1f70
Gomez-mf <gomez-mf@hotmail.com>
2022-02-07 08:01:00 -0300

    se separa stock en otro archivo

---
+++ js/stock.js
@@ -0,0 +1,7 @@
+const waffles = [
+    { id: 1, nombre: "Waffles frutales", tipo: "dulce", precio: 375, img: `./media/wafflefruta.png` },
+    { id: 2, nombre: "Waffles con golosinas", tipo: "dulce", precio: 350, img: `./media/wafflegolosinas.png` },
+    { id: 3, nombre: "Waffles con oreos", tipo: "dulce", precio: 350, img: `./media/waffleoroes.png` },
+    { id: 4, nombre: "Waffle de jamón y queso", tipo: "salado", precio: 400, img: `./media/jyq.png` },
+    { id: 5, nombre: "Waffles de jamón crudo y rúcula", tipo: "salado", precio: 500, img: `./media/rucula.png` },
+];
